46e1d70a789be5ae07da359f9be1ccfcd115d2a4
JLU2016 <626242686@qq.com>
2023-07-16 12:29:39 +0800

    hello 1

--- temp.js
+++ temp.js
@@ -2,10 +2,11 @@
 // @name         hello 
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  new scription
 // @author       JLU2016
 // @match        https://greasyfork.org/zh-CN/users/1127986-jlu2016
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=greasyfork.org
+// @license      AGOL-3.0
 // @grant        none
 // ==/UserScript==
 
